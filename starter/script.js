a0d8e2f74a442ff188b7184318346cb9111e93a3
deividas strole <deiwis@yahoo.com>
2025-01-02 20:06:29 -0800

    w

--- starter/script.js
+++ starter/script.js
@@ -237,9 +237,10 @@ console.log(balance);
 */
 
 // Maximum val
-
+/*
 const max = movements.reduce((acc, mov) => {
   if (acc > mov) return acc;
   else return mov;
 }, movements[0]);
 console.log(max);
+*/
